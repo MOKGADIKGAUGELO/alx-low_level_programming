a0e14458cd3eb990949f857a108ef048d1e9035f
MOKGADIKGAUGELO <khadjoraps@gmail.com>
2022-06-07 08:39:07 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,17 @@
-#include "_putchar"
-
+#include "main.h"
 /**
- * main - Write a program that prints Holberton, followed by a new line.
- *
- * Return: Always 0.
+ * main - prints the string "_putchar" from a character array.
+ * Return: 0 on success.
  */
-
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char c[] = "_putchar";
+	int i;
 
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
